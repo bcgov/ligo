7723236711054bee907f1833f4d5a6ee8f1cc9ff
Brandon Sharratt <brandon@highwaythreesolutions.com>
2017-07-06 15:43:18 -0700

    debug echos

--- web-app/compose/django/start-dev.sh
+++ web-app/compose/django/start-dev.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+echo "Make migrations datasets"
 python manage.py makemigrations datasets
+echo "Make migrations linking"
 python manage.py makemigrations linking
+echo "Migrating"
 python manage.py migrate
 #Could alternatively be started from here
+echo "Starting server"
 python manage.py runserver_plus 0.0.0.0:8000
 
