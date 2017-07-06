694fc430472f731fc94986ecd5efd0d97babeec9
Brandon Sharratt <brandon@highwaythreesolutions.com>
2017-07-06 15:19:38 -0700

    migrations

--- web-app/compose/django/start-dev.sh
+++ web-app/compose/django/start-dev.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-python manage.py makemigrations
+python manage.py makemigrations datasets
+python manage.py makemigrations linking
 python manage.py migrate
 #Could alternatively be started from here
 python manage.py runserver_plus 0.0.0.0:8000
